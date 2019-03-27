ad41632f21229e81d1fb0f3688e3f63e18e81c2b
aliyek <aliwajhah1@gmail.com>
2019-03-27 08:35:47 +0700

    ali
    ini update

--- CI.3.E41170291.P2/application/config/config.php
+++ CI.3.E41170291.P2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI.3.E41170291.P2/';
+$config['base_url'] = 'http://localhost/htdocs/CI.3.E41170291.P2/';
 
 /*
 |--------------------------------------------------------------------------
